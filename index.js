c9f8b9284fa9152279443d4607b0cb167f8334b1
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-28 19:18:28 +0200

    result

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('funguju!');
+
+const form = document.querySelector('.controls');
+const seconds = document.querySelector('.alarm__seconds');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  document.querySelector('button').disabled = true;
+  const inputData = document.querySelector('.time-input');
+  let time = inputData.value;
+  const intervalId = setInterval(() => {
+    time = time - 1;
+    if (time === 0) {
+      clearInterval(intervalId);
+    }
+    seconds.textContent = time;
+  }, 1000);
+});
